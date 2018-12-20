676ee5ecbc380d205dbc7b2d066ddcd948da6f19
darya <kokorina.darya1509@gmail.com>
2018-12-20 21:30:31 +0300

    hw#19 fixed

--- 19/main.js
+++ 19/main.js
@@ -28,41 +28,12 @@ var data = [
 // 'item 1 some text 1'
 // 'item 1 some text 4'
 
-var addText = 'item 1 ';
-
-//usage filter and map functions
-
-var newData = data.map(function(item) {
-    return addText + item.text
+data.filter(function(item) {
+    if (item.display && !item.disable) {
+        return item;
+    }
+}).map(function(item) {
+    return 'item 1 ' + item.text;
+}).forEach(function(item) {
+    console.log(item);
 });
-
-
-function filteredResult1(item) {
-
-    var lastSymbol = item.substr(item.length - 1);
-    return lastSymbol < 2;
-}
-
-var result1 = newData.filter(filteredResult1);
-
-console.log(result1[0]);
-
-
-
-//usage filter and forEach functions
-
-data.forEach(function(item) {
-    item.text = addText + item.text;
-});
-
-
-function filteredResult2(item) {
-    var lastSymbol = item.text.substr(item.text.length - 1);
-    return lastSymbol > 3;
-}
-
-
-var result2 = data.filter(filteredResult2);
-
-console.log(result2[0].text);
-
